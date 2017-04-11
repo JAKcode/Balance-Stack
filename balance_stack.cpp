78b4afb37383afdd9de334404218fa4da2195bdb
redherring96 <bhiggins744@gmail.com>
2017-04-10 23:02:32 -0600

    balance_stack.cpp
    cpp for the balancing parenthesis assignment

---
+++ balance_stack.cpp
@@ -0,0 +1,42 @@
+//# Balance-Stack
+
+//now functional
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+void push (string & x)
+{
+stack <char> stk;
+for(unsigned int i = 0; i < x.length() -1; i++)
+{
+if ((stk[i])=='(')
+{
+	stk.push(x);
+}
+else if (stk[i]==')')
+{
+	if (stk.empty())
+	{
+		cout <<"underflow!!";
+	}
+	else
+	{
+		stk.pop();
+	}
+}
+if (stk[i] == NULL)
+{
+	cout <<"balanced expr!!";
+}
+else 
+    cout <<"Incorrect String";
+}
+}
+int main()
+{	
+string input;
+cin >> input;
+push(input);
+system("pause");
+}
